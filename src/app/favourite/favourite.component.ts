ec540c8c0b4217105339dbc49e8329c9e5d97431
Sourav Kurmi <sourav.patel2021@vitstudent.ac.in>
2024-11-08 19:13:17 +0530

    modified:   src/app/favourite/favourite.component.html 	modified:   src/app/favourite/favourite.component.ts

--- src/app/favourite/favourite.component.ts
+++ src/app/favourite/favourite.component.ts
@@ -9,23 +9,34 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './favourite.component.html',
-  styleUrl: './favourite.component.scss'
+  styleUrls: ['./favourite.component.scss']
 })
 export class FavouriteComponent {
+  favorites: any[] = [];
 
-  favorites:any[]=[]
-  constructor(private favouriteService:FavouriteService,
-    private restaurantService:RestaurantService,
-    private router:Router
-  ){}
+  constructor(
+    private favouriteService: FavouriteService,
+    private restaurantService: RestaurantService,
+    private router: Router
+  ) {}
 
-  ngOnInit(){
-    const favoriteNames=this.favouriteService.getFavourites();
-    this.favorites=this.restaurantService.getRestuarantsByName(favoriteNames);
+  ngOnInit() {
+    const favoriteNames = this.favouriteService.getFavourites();
+    this.favorites = this.restaurantService.getRestuarantsByName(favoriteNames);
     console.log(this.favorites);
   }
-  navigateToOrders(restaurant:any){
+
+  navigateToOrders(restaurant: any) {
     this.restaurantService.setRestaurantInfo(restaurant);
     this.router.navigate(['/orders']);
+  }
+
+  removeFromFavorites(index: number) {
+    const restaurant = this.favorites[index];
+    if (restaurant) {
+      this.favouriteService.removeFavourite(restaurant.title);
+      this.favorites.splice(index, 1);
+      console.log('Removed', restaurant.title);
+    }
   }
 }
